f31a9b8df1e0bb0615562c302b2b04cc34157764
Gabriel Ferraz <gabrielsh38s2@gmail.com>
2024-12-14 11:52:56 -0300

    List view, crete

--- funcionario/app/urls.py
+++ funcionario/app/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
+from .views import FuncionarioCreateView, FuncionarioListView
+
 urlpatterns = [
-    
+    path("form_funcionario", FuncionarioCreateView.as_view()),
+    path('lista_funcionarios', FuncionarioListView.as_view(), name = "lista_funcionarios"),
 ]
--- funcionario/app/views.py
+++ funcionario/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import CreateView, ListView
+from .models import Funcionario
+
+class FuncionarioCreateView(CreateView):
+    model = Funcionario
+    fields = "__all__"
+    template_name = "form_funcionario.html"
+
+class FuncionarioListView(ListView):
+    model = Funcionario
+    template_name = "lista_funcionarios.html"
